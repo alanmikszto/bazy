476c0ad10a4a624b891ae28f0f22927ac4017e4e
alanmikszto <73993357+alanmikszto@users.noreply.github.com>
2020-12-21 18:06:40 +0100

    Update zadania.sql

--- zadania.sql
+++ zadania.sql
@@ -274,4 +274,4 @@ CWICZENIA - 3
 1. SELECT imie, nazwisko, YEAR(data_urodzenia) as rok_urodzenia from pracownik;
 2. SELECT imie, nazwisko, YEAR(current_date())-YEAR(data_urodzenia) as wiek from pracownik;
 3. select dzial.nazwa, count(pracownik.id_pracownika) as ilosc from dzial join pracownik on pracownik.dzial = dzial.id_dzialu group by dzial.nazwa;
-
+4. select kategoria.nazwa_kategori, sum(stan_magazynowy.ilosc) from kategoria join towar on  kategoria.id_kategori=towar.kategoria join stan_magazynowy on towar.id_towaru=stan_magazynowy.towar group by kategoria.nazwa_kategori;
